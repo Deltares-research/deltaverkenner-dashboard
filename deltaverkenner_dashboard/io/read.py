76cf21de1459613077e7168f78fb8645f7c721a0
jreusen <25225054+JesseReusen@users.noreply.github.com>
2026-05-05 15:42:59 +0200

    Write run values to excel output file

--- deltaverkenner_dashboard/io/read.py
+++ deltaverkenner_dashboard/io/read.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import re
 
 import pandas as pd
+import numpy as np
 
 
 def read_excel_sheet(path: Path, sheet_name: str):
@@ -10,11 +12,102 @@ def read_excel_sheet(path: Path, sheet_name: str):
     return data
 
 
+def replace_region(col):
+    match = re.search(r"(r\d+)$", col)
+    if match:
+        r = match.group(1)
+        return col.replace(r, regiokoppeling.get(r, r))
+    return col
+
+
 if __name__ == "__main__":
-    path_to_datafile = Path(
-        "p:/11212687-deltaverkenner2026/Zoetwater/deltaverkenner-data/data/3-input/demand.xlsx"
+    # path_to_datafile = Path(
+    #     "p:/11212687-deltaverkenner2026/Zoetwater/deltaverkenner-data/data/3-input/demand.xlsx"
+    # )
+
+    # sheet_name = "BP18REF2017_slr0"
+
+    # data = read_excel_sheet(path_to_datafile, sheet_name)
+
+    # runs = [1, 2]
+    runs = np.linspace(1, 21, 21).astype(int)
+
+    regiokoppeling = {
+        "r10": "h1",
+        "r11": "h1",
+        "r12": "h1",
+        "r13": "h1",
+        "r14": "h3",
+        "r15": "h3",
+        "r16": "h2",
+        "r17": "h2",
+        "r18": "h2",
+        "r19": "h6",
+        "r20": "h4",
+        "r21": "h4",
+        "r1": "h5",
+        "r2": "h5",
+        "r3": "h5",
+        "r4": "h5",
+        "r5": "h6",
+        "r6": "h6",
+        "r7": "h1",
+        "r8": "h1",
+        "r9": "h1",
+    }
+
+    index_col = 0
+
+    nr_of_deelregios = 21
+    # priorities = np.linspace(1, 5, 5).astype(int)
+    priorities = np.linspace(1, 4, 4).astype(int)
+    types = ["Demand", "Allocation", "Shortage"]
+
+    selected_years = [1976, 2003]
+    selected_months = [4, 5, 6, 7, 8, 9]
+
+    column_names = []
+
+    for p in priorities:
+        for t in types:
+            for i in range(nr_of_deelregios):
+                column_names.append(f"p{p}_{t}_r{i+1}")
+
+    # add the unnamed index column name to the list of column names
+    column_names = ["time"] + column_names
+
+    writer = pd.ExcelWriter(
+        Path(
+            "p:/11212687-deltaverkenner2026/Zoetwater/Dashboard/data/2-output/dashboard_2026.xlsx"
+        )
     )
 
-    sheet_name = "BP18REF2017_slr0"
+    for run in runs:
 
-    data = read_excel_sheet(path_to_datafile, sheet_name)
+        print(f"Working on run {run}")
+
+        path_to_datafile = Path(
+            f"p:/11212687-deltaverkenner2026/Zoetwater/deltaverkenner-data/data/3-results/long/{run}.csv"
+        )
+
+        data = pd.read_csv(path_to_datafile, index_col=index_col, usecols=column_names)
+
+        data.index = pd.to_datetime(data.index)
+
+        data_year = data[data.index.year.isin(selected_years)]
+
+        selected_data = data_year[data_year.index.month.isin(selected_months)]
+
+        # replace the deelregio name with the hoofdregio name
+        # selected_data.columns = selected_data.columns.str.replace(regiokoppeling)
+        selected_data.columns = selected_data.columns.map(replace_region)
+
+        selected_data_grouped = (
+            selected_data.T.groupby(by=selected_data.columns).sum().T
+        )
+
+        # sheet = writer.sheets[f"Run {run}"]
+
+        selected_data_grouped.to_excel(writer, sheet_name=f"Run {run}")
+
+    writer.close()
