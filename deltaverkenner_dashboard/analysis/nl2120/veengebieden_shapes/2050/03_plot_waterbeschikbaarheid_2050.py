2fbec14319f653a8ec55be71f90ca226cf5d8709
jreusen <25225054+JesseReusen@users.noreply.github.com>
2026-08-20 17:09:22 +0200

    change path

--- deltaverkenner_dashboard/analysis/nl2120/veengebieden_shapes/2050/03_plot_waterbeschikbaarheid_2050.py
+++ deltaverkenner_dashboard/analysis/nl2120/veengebieden_shapes/2050/03_plot_waterbeschikbaarheid_2050.py
@@ -208,6 +208,6 @@ for watervraag_type in watervraag_types:
             columns={"Watervraag gecorrigeerd": "Waterbeschikbaarheid (m3/s)"}
         )
 
-        outputpath = f"p:/11212687-deltaverkenner2026/Zoetwater/Dashboard/data/nl2120/figuren/shapes_veengebieden/2050/csv's Dimmie/03_waterbeschikbaarheid_{run}_watervraag_{watervraag_type.lower()}_veengebieden_{selected_month}_1976.csv"
+        outputpath = f"p:/11212687-deltaverkenner2026/Zoetwater/Dashboard/data/nl2120/figuren/shapes_veengebieden/2050/csv's Dimmie/03_waterbeschikbaarheid_2050_veengebieden_{selected_month}_1976.csv"
 
         deelregios_with_watervraag.to_csv(outputpath, index=False)
